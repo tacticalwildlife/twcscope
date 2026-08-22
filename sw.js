011a62bf8abf9290ac2af5832d51cae12228b95f
tacticalwildlife <tacticalwildlifecontrol@gmail.com>
2026-08-22 09:29:51 -0500

    Network-first SW + bump cache to v33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'twc-scope-v32';
+const CACHE = 'twc-scope-v33';
 const ASSETS = [
   './',
   './index.html',
@@ -25,12 +25,12 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r =>
-      r || fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match('./index.html'))
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() =>
+      caches.match(e.request).then(r => r || caches.match('./index.html'))
     )
   );
 });
